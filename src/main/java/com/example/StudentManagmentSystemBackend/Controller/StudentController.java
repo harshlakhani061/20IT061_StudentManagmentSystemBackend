ef84eddd3350da1d085b9962791e5a0cd6adf046
Harsh Lakhani <20it061@charusat.edu.in>
2022-09-07 19:34:09 +0530

    Added Cross Origin

--- src/main/java/com/example/StudentManagmentSystemBackend/Controller/StudentController.java
+++ src/main/java/com/example/StudentManagmentSystemBackend/Controller/StudentController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
+@CrossOrigin(origins="https://app-dummy123.herokuapp.com")
+
 
 public class StudentController {
 
